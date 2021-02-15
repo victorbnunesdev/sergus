f4fc5c63ef00d574ad6c005914f39829e324a2f7
Thiago de Freitas Maranhão <thiagofmaranhao@gmail.com>
2021-02-15 17:26:05 -0300

    Tela Sobre: Adicionando botões de rodapé

--- sergusapp/lib/app/views/sobre/sobre_view.dart
+++ sergusapp/lib/app/views/sobre/sobre_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sergusapp/app/views/sobre/inicio_partial_view.dart';
 import 'package:sergusapp/app/views/sobre/politica_privacidade_partial_view.dart';
 import 'package:sergusapp/app/views/sobre/sergus_educacao_partial_view.dart';
-import 'package:sergusapp/app/views/sobre/sobre_botoes_partial_view.dart';
 import 'package:sergusapp/app/views/sobre/ultimas_noticias_partial_view.dart';
 import 'package:sergusapp/constants.dart';
 
@@ -28,10 +27,17 @@ class SobreView extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: preencherSessaoInterna(sobrePartialViews),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: preencherSessaoInterna(sobrePartialViews),
+            ),
+          ),
+          preencherRodape(context),
+        ],
       ),
-      floatingActionButton: SobreBotoesPartialView(),
+      //floatingActionButton: SobreBotoesPartialView(),
     );
   }
 }
@@ -55,4 +61,117 @@ preencherSessaoInterna(SobrePartialViews sobrePartialViews) {
     default:
       return InicioPartialView();
   }
+}
+
+preencherRodape(BuildContext context) {
+  return Row(
+    children: [
+      Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          height: 100,
+          child: FlatButton(
+            color: kPrimaryColor,
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SobreView(
+                      sobrePartialViews: SobrePartialViews.ultimas_noticias),
+                ),
+              );
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.new_releases_outlined,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Últimas Notícias',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          height: 100,
+          child: FlatButton(
+            color: kPrimaryColor,
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SobreView(
+                      sobrePartialViews: SobrePartialViews.sergus_educacao),
+                ),
+              );
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.school_outlined,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Sergus Educação',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          height: 100,
+          child: FlatButton(
+            color: kPrimaryColor,
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SobreView(
+                      sobrePartialViews:
+                          SobrePartialViews.politica_de_privacidade),
+                ),
+              );
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.privacy_tip_outlined,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Política de Privacidade',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
 }
