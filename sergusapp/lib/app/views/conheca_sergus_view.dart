d2561bbae52d21b86ad5dcacb59353cb0a113dcd
Thiago de Freitas Maranhão <thiagofmaranhao@gmail.com>
2021-02-17 21:19:50 -0300

    Início implementação da tela Conheça a Sergus

---
+++ sergusapp/lib/app/views/conheca_sergus_view.dart
@@ -0,0 +1,328 @@
+import 'package:flutter/material.dart';
+import 'package:sergusapp/constants.dart';
+
+class ConhecaSergusView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: appBarToolbarHeight,
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Conheça a Sergus',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          _carregarSessaoConhecaSergus(),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'Informações e Serviços',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          _carregarSessaoInformacoesServicos(),
+        ]),
+      ),
+    );
+  }
+}
+
+_carregarSessaoConhecaSergus() {
+  return Column(
+    children: [
+      Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.present_to_all,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Apresentação',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.book_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Cartilha do Participante',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.school_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Sergus Educação',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.privacy_tip_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Política de Privacidade',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.rule_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Regulamento',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+_carregarSessaoInformacoesServicos() {
+  return Column(
+    children: [
+      Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.calculate_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Simulador',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.speaker_notes_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Comunicados',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.contact_phone_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Contatos',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+              child: FlatButton(
+                color: kSecondColor,
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.article_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    Text(
+                      'Últimas Notícias',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 120,
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
--- sergusapp/lib/constants.dart
+++ sergusapp/lib/constants.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF0B6E45);
 const kPrimarylIGHTColor = Color(0xFFF1E6FF);
+const kSecondColor = Color(0xFFE8F5E9);
 const double appBarToolbarHeight = 100;
